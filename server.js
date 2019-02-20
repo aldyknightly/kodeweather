5100246111ce383a15f9e38150efbf3a457fd128
ardhiraka <mailtorakaa@gmail.com>
2019-02-20 12:05:12 +0700

    add env port

--- server.js
+++ server.js
@@ -31,6 +31,6 @@ app.post('/', function(req, res) {
     })
 });
 
-app.listen(5000, function() {
+app.listen(process.env.PORT || 8080, function() {
     console.log("Kode weather listening on port 8080")
 })
